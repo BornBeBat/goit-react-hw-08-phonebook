cdc2f0831b9b77c92955eec18695f08cccb3da79
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-01-30 17:18:08 +0200

    delete index in pages

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { Home as default} from './Home/Home';
-export { Register as default } from './Register/Register';
-export { Login as default } from './Login/Login';
-export { Contacts as default } from './Contacts/Contacts';
